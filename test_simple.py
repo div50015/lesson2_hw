89dd22d79256f6212407afbe1e218986d3552240
div50015 <div50015@mail.ru>
2023-09-19 16:35:56 +0300

    Тест с pytest + fixture

--- test_simple.py
+++ test_simple.py
@@ -1,5 +1,26 @@
-def test_first():
-    assert 1 == 1
+from selene.support.shared import browser
+from selene import be, have
+import pytest
 
-def test_second():
-    pass
+@pytest.fixture(scope="session")
+def browser_f():
+#    pass
+    browser.driver.set_window_size(800,600)
+
+
+def test_first(browser_f):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+def test_second(browser_f):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('erhsdffhyurtru').press_enter()
+    browser.element('[id="search"]').should(have.text('erhsdffhyurtru'))
+
+
+
+
+#browser.open('https://google.com')
+#browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+#browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
